7b799c21582ce426b3bbf8e6c02ba0f685ceef6f
Gabriel Couto <gabrielcouto26@gmail.com>
2024-08-16 00:10:35 -0300

    fix initializer.sh

--- initializer.sh
+++ initializer.sh
@@ -5,10 +5,12 @@ echo "Initializing..."
 # Checking storage file
 if [ ! -f ./storage.json ]; then
   echo "No storage file found. Creating file..."
-  if ! touch ./storage.json && echo "{}" >> ./storage.json &>/dev/null; then
+  if ! touch ./storage.json &>/dev/null; then
     echo "Error creating storage file."
     exit 1
   fi
+
+  echo "{}" >> .storage.json
 
   echo "Done."
   echo
@@ -18,6 +20,8 @@ fi
 # Checking PASS_STORAGE_KEY environemtn variable
 if [ -z "${PASS_STORAGE_KEY}" ]; then
   echo "PASS_STORAGE_KEY environment variable not set."
+  # TODO: melhorar isso aqui
+  echo "Make sure to set PASS_STORAGE_KEY and try again."
   exit 1
 fi
 
@@ -50,6 +54,6 @@ echo
 echo "Recommendation:
 Create an alias for better usage.
 Add the code below to your rc file and use it as a true CLI.
-``alias passenc='path/to/module/dist/index.js'``"
+``alias passenc='node path/to/module/dist/index.js'``"
 
 echo
